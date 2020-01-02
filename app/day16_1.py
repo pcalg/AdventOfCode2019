dcbb29e0a20f43b23c70ed5fc186f9f761d53645
pcalg <pcalgra@hotmail.com>
2020-01-02 21:40:04 +0100

    day16

---
+++ app/day16_1.py
@@ -0,0 +1,63 @@
+"""
+solution AdventOfCode 2019 day 16 part 1.
+
+https://adventofcode.com/2019/day/16.
+
+author: pca
+
+"""
+
+from general.general import read_file
+from general.general import get_location_input_files
+from general.general import measure
+from itertools import cycle
+
+
+def make_pattern(base_pattern, repeats):
+    first = True
+
+    for element in cycle(base_pattern):
+        for _ in range(repeats + 1):
+            if first:
+                first = False
+            else:
+                yield element
+
+
+def next_signal(base_pattern, signal):
+
+    signal_next = []
+
+    for idx, element in enumerate(signal):
+        total = 0
+
+        for s, p in zip(signal, make_pattern(base_pattern, idx)):
+            total += s * p
+
+        signal_next.append(abs(total) % 10)
+    return signal_next
+
+
+@measure
+def run_all(signal):
+    pattern = [0, 1, 0, -1]
+
+    for _ in range(100):
+        signal = next_signal(pattern, signal)
+
+    return signal
+
+
+def main(args=None):
+
+    input_signal = read_file(get_location_input_files(), 'input_day16.txt')[0]
+
+    signal = [int(ch) for ch in input_signal]
+
+    signal_result = run_all(signal)
+
+    print("".join([str(d) for d in signal_result[0:8]]))
+
+
+if __name__ == "__main__":
+    main()
--- general/general.py
+++ general/general.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from functools import wraps
+from time import time
 
 
 def read_file(file_location, file_name):
@@ -38,3 +40,15 @@ def save_output(file_location, file_name, lines):
     fn = file_location / file_name
     with open(fn, 'w') as f:
         f.write('\n'.join(lines) + '\n')
+
+
+def measure(func):
+    @wraps(func)
+    def _time_it(*args, **kwargs):
+        start = int(round(time() * 1000))
+        try:
+            return func(*args, **kwargs)
+        finally:
+            end_ = int(round(time() * 1000)) - start
+            print(f"Total execution time: {end_ if end_ > 0 else 0} ms")
+    return _time_it
