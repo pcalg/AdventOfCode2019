0d8e1a25bdb7951410b03de1596b89fe4996b951
pcalg <pcalgra@hotmail.com>
2020-01-19 21:36:57 +0100

    day 18_1

---
+++ app/day18_1.py
@@ -0,0 +1,128 @@
+"""
+solution AdventOfCode 2019 day 18 part 1.
+
+https://adventofcode.com/2019/day/18.
+
+author: pca
+
+"""
+
+from general.general import read_file, get_location_input_files, measure
+import heapq
+
+
+def create_grid2d(grid_txt):
+    skip_ch = ['#', '.']
+    grid2d = dict()
+    nodes = dict()
+
+    for y, line in enumerate(grid_txt):
+        for x, ch in enumerate(line):
+            grid2d[(y, x)] = ch
+            if ch not in skip_ch:
+                nodes[ch] = (y, x)
+
+    return grid2d, nodes
+
+
+def find_distances(grid2d, node_ch, node_pos):
+    """
+    Find the distances using BFS from node_ch to the next reachable node (key or door).
+    """
+    distances = dict()
+    node_neighbours = set()
+
+    deltas = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    q = list()
+    q.append((0, node_pos))
+    visited = set()
+
+    while len(q) > 0:
+        d, pos = q.pop(0)
+        visited.add(pos)
+
+        # find neighbours
+        y, x = pos
+        for dy, dx in deltas:
+            neighbour_pos = y + dy, x + dx
+            neighbour_ch = grid2d[neighbour_pos]
+
+            # only move to a next nodes if it's an empty space
+            if neighbour_ch == '.' and neighbour_pos not in visited:
+                q.append((d + 1, neighbour_pos))
+
+            # we don't want to store the empty cells or doors as nodes
+            if neighbour_ch not in ['.', '#'] and neighbour_pos not in visited:
+                distances[(node_ch, neighbour_ch)] = d + 1
+                node_neighbours.add(neighbour_ch)
+
+    return distances, node_neighbours
+
+@measure
+def solve(distances, neighbours):
+    """
+    Find the minimum distance needed to find all the keys.
+
+    :param distances:  dictionary that contains the length of the edges {(from, to): distance, ...}
+    :param neighbours: dictionary that contains the neighbours for each node
+    :return: minimum distance
+    """
+
+    n_keys = len([k for k in neighbours.keys() if k.islower()])
+
+    frontier = list()
+    visited = set()
+
+    heapq.heappush(frontier, (0, '@', []))
+
+    while len(frontier) > 0:
+        d, node, keys = heapq.heappop(frontier)
+
+        # Skip if already been in this state
+        if (node, str(keys)) not in visited:
+            visited.add((node, str(keys)))
+
+            if len(keys) == n_keys:
+                print(f"Distance: {d}")
+                return d
+
+            nb_list = neighbours[node]
+
+            for neighbour in nb_list:
+                # When at a door, we need a key to go through, otherwise we can always pass.
+                can_pas = (neighbour.isupper() and neighbour.lower() in keys) or (not neighbour.isupper())
+
+                if can_pas:
+                    # Found a new key, then add it to our keys
+                    if neighbour.islower() and neighbour not in keys:
+                        neighbour_keys = sorted([key for key in keys] + [neighbour])
+                    else:
+                        neighbour_keys = [key for key in keys]
+
+                    heapq.heappush(frontier, (d + distances[(node, neighbour)], neighbour, neighbour_keys))
+
+    return None
+
+
+def main(args=None):
+    grid_txt = read_file(get_location_input_files(), 'input_day18.txt')
+
+    grid2d, nodes = create_grid2d(grid_txt)
+
+    distances = dict()
+    neighbours = dict()
+
+    # for each node use BFS to find the reachable other nodes
+    for node in nodes:
+        d, node_neighbours = find_distances(grid2d, node, nodes[node])
+        distances.update(d)
+        neighbours[node] = node_neighbours
+
+    dist = solve(distances, neighbours)
+
+    print(f"Distance: {dist}")
+
+
+if __name__ == "__main__":
+    main()
